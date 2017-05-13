acd844b65b85dcfeeb9be9a747bba7fead44047e
hyunji-bang <groovehyunji@gmail.com>
2017-05-13 12:01:02 +0900

    cacheDirectory 설정 false로 변경, title 추가

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
                     loader: 'babel-loader',
                     exclude: /node_modules/,
                     query: {
-                        cacheDirectory: true,
+                        cacheDirectory: false,
                         plugins: ['transform-class-properties'],
                         presets: ['es2015', 'react']
                     }
