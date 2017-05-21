550053e0406584893017a6fe735ed87c974c6fb6
hyunji-bang <groovehyunji@gmail.com>
2017-05-22 06:45:03 +0900

    NoMatch 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import { Wrapper, Header, MovieWrap, ModalWrap } from './components';
 import * as service from './services/post';
 
@@ -7,6 +7,7 @@ import PollList from './routes/PollList';
 import PollItem from './routes/PollItem';
 import NewPoll from './routes/NewPoll';
 import Result from './routes/Result';
+import NoMatch from './routes/Nomatch';
 
 class App extends Component {
     constructor(props){
@@ -18,16 +19,19 @@ class App extends Component {
             <Router>
                 <Wrapper>
                     <Header/>
-                    <Route exact path="/"
-                           render={(props) => <PollList {...props}/>}/>
-                    <Route path="/poll/:id" 
-                           render={(props) => <PollItem {...props} 
-                                              toggleModal={this.toggleModal} 
-                                              />} />
-                    <Route path="/newpoll" 
-                           render={(props) => <NewPoll {...props} 
-                                              sendPollInfo={this.sendPollInfo}/>} />
-                    <Route path="/result/:id" render={(props) => <Result {...props} />}/>
+                    <Switch>
+                        <Route exact path="/"
+                            render={(props) => <PollList {...props}/>}/>
+                        <Route path="/poll/:id" 
+                            render={(props) => <PollItem {...props} 
+                                                toggleModal={this.toggleModal} 
+                                                />} />
+                        <Route path="/newpoll" 
+                            render={(props) => <NewPoll {...props} 
+                                                sendPollInfo={this.sendPollInfo}/>} />
+                        <Route path="/result/:id" render={(props) => <Result {...props} />}/>
+                        <Route component={NoMatch}/>
+                    </Switch>
                 </Wrapper>
             </Router>
         );
